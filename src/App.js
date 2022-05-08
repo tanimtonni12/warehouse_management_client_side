cbabcb2c4602a2e86efe4b941733e16114b50b84
tanimtonni12 <17103062iubat@gmail.com>
2022-05-08 19:15:21 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import AddInventory from './components/AddInventory/AddInventory';
 import Blogs from './components/Blogs/Blogs';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
@@ -30,6 +31,7 @@ function App() {
         <Route path='/myItems' element={<RequireAuth><MyItems></MyItems></RequireAuth>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
       </Routes>
+      <Footer></Footer>
 
     </div >
   );
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import app from '../../images/App Store Badgeapple.png'
+import playStore from '../../images/Google Play Badge.png'
 
 const Footer = () => {
     return (
-        <div>
+        <footer className="container-fluid  bg-dark py-5 ">
+            <div className="container">
 
-        </div>
+                <div className="row d-flex align-items-center text-lg-start text-center ">
+                    <div className="col-lg-10 col-md-6 col-sm-12">
+
+                        <p className="text-white "><small>
+                            © Laptop WareHouse Ltd. All rights reserved
+                        </small></p>
+                    </div>
+                    <div className="col-lg-2 col-md-6 col-sm-12">
+                        <div className="row text-lg-start text-center">
+                            <div className="col-lg-12 col-md-12 mb-3">
+                                <h5 className="text-white">Get The App</h5>
+
+                            </div>
+                            <div className="col-lg-12 col-md-12 col-sm-12 mb-3">
+
+                                <a href="https://www.apple.com/app-store/" target="_blank" rel="noopener noreferrer"><img src={app} alt="" /></a>
+
+                            </div>
+                            <div className="col-lg-12 col-sm-12 col-md-12">
+                                <a href="https://playstore.com/" target="_blank" rel="noopener noreferrer"><img src={playStore} alt="" /></a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
+
+
+        </footer>
     );
 };
 
