1e76d8c27c3a1251fed9c8fff5a4cbdf41b21db1
tanimtonni12 <17103062iubat@gmail.com>
2022-05-09 19:32:53 +0600

    manage inventory updated

--- src/components/ManageInventories/ManageInventories.js
+++ src/components/ManageInventories/ManageInventories.js
@@ -13,7 +13,7 @@ const ManageInventories = () => {
     const handleDelete = id => {
         const proceed = window.confirm('are you sure that you want to delete this item?');
         if (proceed) {
-            const url = `http://localhost:5000/product/${id}`;
+            const url = `https://intense-taiga-61434.herokuapp.com/${id}`;
             fetch(url, {
                 method: 'DELETE'
             })
